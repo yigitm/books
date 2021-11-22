7684de33de2fe3a85a4c45c080c791016dfebdb1
Yigit Mersin <yigitmersin@gmail.com>
2021-11-22 22:03:44 +0300

    Add function-show

--- js/main.js
+++ js/main.js
@@ -2,21 +2,49 @@ const formButton = document.querySelectorAll('form');
 
 let books = [];
 
-const singleBook = {
-  title: '',
-  author: '',
-};
-
-function addBook() {
-  singleBook.title = document.getElementById('title').value;
-  singleBook.author = document.getElementById('author').value;
-  books.push(singleBook);
-  console.log(books);
+class SingleBook {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
+function addBook() {
+  titleValue = document.getElementById('title').value;
+  authorValue = document.getElementById('author').value;
+  book = new SingleBook(titleValue, authorValue);
+  books.push(book);
+  addLocal();
+  //console.log(books);
+}
+
+function addLocal() {
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function getLocal() {
+  books = JSON.parse(localStorage.getItem('books'));
+}
+
+function remove() {}
+
+function show() {
+  getLocal();
+  const ul = document.getElementById('display-books');
+  books.forEach(element => {
+    const li = document.createElement('li');
+    ul.append(li);
+    li.innerText = element.title;
+    li.innerText = '<br />';
+    li.innerText = element.author;
+  });
+}
 addEventListener('submit', e => {
   e.preventDefault();
   if (e.currentTarget) {
     addBook();
   }
+  show();
 });
+
+window.addEventListener('DOMContentLoaded', event => {});
